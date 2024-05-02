0370c96fa626318d611316dea6bb38be5861c802
Daniels Vidulejs <d.vidulejs@tum.de>
2024-05-02 15:48:43 +0200

    calculate_dt

--- src/Fields.cpp
+++ src/Fields.cpp
@@ -58,13 +58,18 @@ double Fields::calculate_dt(Grid &grid) {
     double dx_2 = grid.dx() * grid.dx();
     double dy_2 = grid.dy() * grid.dy();
 
-    double u_max = std::abs(_U.max_value());
-    double v_max = std::abs(_V.max_value());
+    double u_max = _U.max_value();
+    double u_min = _U.min_value();
+    u_max = std::max(u_max, std::abs(u_min));
+
+    double v_max =_V.max_value();
+    double v_min =_V.min_value();
+    v_max = std::max(v_max, std::abs(v_min));
 
     double coefficient = (dx_2 * dy_2) / (dx_2 + dy_2);
     _dt = std::min({coefficient / (2 * _nu), grid.dx() / u_max, grid.dy() / v_max});
 
-    return _dt;
+    return _tau*_dt;
 }
 
 double &Fields::p(int i, int j) { return _P(i, j); }
