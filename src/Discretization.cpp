f91e36756f350385aeed28350d72c4f0fc48eeda
Daniels Vidulejs <d.vidulejs@tum.de>
2024-05-27 19:17:06 +0200

    BUGFIX Append std:: to pow() and abs()

--- src/Discretization.cpp
+++ src/Discretization.cpp
@@ -13,7 +13,7 @@ Discretization::Discretization(double dx, double dy, double gamma) {
 }
 
 double Discretization::convection_u(const Matrix<double> &U, const Matrix<double> &V, int i, int j) {
-    double du2_dx = 1 / _dx * (pow(interpolate(U, i, j, 1, 0), 2) - pow(interpolate(U, i, j, -1, 0), 2)) +
+    double du2_dx = 1 / _dx * (std::pow(interpolate(U, i, j, 1, 0), 2) - std::pow(interpolate(U, i, j, -1, 0), 2)) +
                     _gamma / _dx *
                         ((std::abs(interpolate(U, i, j, 1, 0)) * (U(i, j) - U(i + 1, j)) / 2) -
                          std::abs(interpolate(U, i, j, -1, 0)) * (U(i - 1, j) - U(i, j)) / 2);
@@ -28,7 +28,7 @@ double Discretization::convection_u(const Matrix<double> &U, const Matrix<double
 }
 
 double Discretization::convection_v(const Matrix<double> &U, const Matrix<double> &V, int i, int j) {
-    double dv2_dy = 1/ _dy * (pow(interpolate(V,i,j,0,1), 2) - pow(interpolate(V,i,j - 1,0,1), 2)) +
+    double dv2_dy = 1/ _dy * (std::pow(interpolate(V,i,j,0,1), 2) - std::pow(interpolate(V,i,j - 1,0,1), 2)) +
                     _gamma/_dy * ((std::abs(interpolate(V,i,j,0,1)) * (V(i, j) - V(i, j + 1)) / 2) -
                                     std::abs(interpolate(V, i, j - 1, 0, 1)) * (V(i, j - 1)- V(i, j)) / 2) ;
     double du2_dx = 1/ _dx * (((interpolate(U, i, j, 0, 1)) * (interpolate(V, i, j, 1, 0)))  - ( (interpolate(U,i-1,j,0,1)) * (interpolate(V,i-1,j,1,0)))) +
@@ -39,14 +39,15 @@ double Discretization::convection_v(const Matrix<double> &U, const Matrix<double
 }
 
 double Discretization::convection_t(const Matrix<double> &T, const Matrix<double> &U, const Matrix<double> &V, int i, int j) {
-    return 1/_dx * (U(i,j)* interpolate(T,i,j,1,0) - U(i-1,j)* interpolate(T,i,j,-1,0)) 
-    + _gamma/_dx * (abs(U(i,j))* (T(i,j)-T(i+1,j))/2 - (abs(U(i-1,j))*(T(i-1,j)-T(i,j))/2))
-    + 1/_dy * (V(i,j)* interpolate(T,i,j,0,1) - V(i,j-1)* interpolate(T,i,j,0,-1)) 
-    + _gamma/_dy * (abs(V(i,j))* (T(i,j)-T(i,j+1))/2 - (abs(V(i,j-1))*(T(i,j-1)-T(i,j))/2));
+    double duT_dx = 1/_dx * (U(i,j)* interpolate(T,i,j,1,0) - U(i-1,j)* interpolate(T,i,j,-1,0))
+                    + _gamma/_dx * (std::abs(U(i,j))* (T(i,j)-T(i+1,j))/2 - std::abs(U(i-1,j))*(T(i-1,j)-T(i,j))/2);
+    double dvT_dy = 1/_dy * (V(i,j) * interpolate(T,i,j,0,1) - V(i,j-1)* interpolate(T,i,j,0,-1))
+                    + _gamma/_dy * (std::abs(V(i,j))* (T(i,j)-T(i,j+1))/2 - std::abs(V(i,j-1))*(T(i,j-1)-T(i,j))/2);
+    return duT_dx + dvT_dy;
 }
 
 double Discretization::laplacian(const Matrix<double> &A, int i, int j) {
-    return (A(i+1,j) - 2*A(i,j) + A(i-1,j))/pow(_dx,2) + (A(i,j+1) - 2*A(i,j) + A(i,j-1))/pow(_dy,2);
+    return (A(i+1,j) - 2*A(i,j) + A(i-1,j))/std::pow(_dx,2) + (A(i,j+1) - 2*A(i,j) + A(i,j-1))/std::pow(_dy,2);
 }
 
 double Discretization::sor_helper(const Matrix<double> &P, int i, int j) {
