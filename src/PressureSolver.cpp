617d88eb8204f2767d3f4833e6a018a921177972
Daniels <vidulejs2@gmail.com>
2024-06-24 22:12:11 +0200

    Execute cuda kernel for pressure solver. Pointer magic for swapping pointers.

--- src/PressureSolver.cpp
+++ src/PressureSolver.cpp
@@ -42,8 +42,11 @@ double SOR::solve(Fields &field, Grid &grid, const std::vector<std::unique_ptr<B
     return res;
 }
 
-__global__ void jacobiKernel(double *d_p_matrix_new, double *d_p_matrix, const double *d_rs_matrix, const bool *d_fluid_mask,
+__global__ void jacobiKernel(double **d_p_matrix_new_dev, double **d_p_matrix_dev, const double *d_rs_matrix, const bool *d_fluid_mask,
                              const double coeff, const int imax, const int jmax, const double dx, const double dy) {
+
+    double* d_p_matrix_new = *d_p_matrix_new_dev;
+    double* d_p_matrix = *d_p_matrix_dev;
     int i = blockIdx.x * blockDim.x + threadIdx.x + 1;
     int j = blockIdx.y * blockDim.y + threadIdx.y + 1;
     int idx = i + j * (imax + 2);
@@ -61,23 +64,16 @@ __global__ void jacobiKernel(double *d_p_matrix_new, double *d_p_matrix, const d
 }
 
 
-////call with <<<1, 1>>> to swap pointers
-//__global__ void swapPointers(double *d_p_matrix, double *d_p_matrix_new) {
-//    __syncthreads();
-//    double *temp = d_p_matrix;
-//    d_p_matrix = d_p_matrix_new;
-//    d_p_matrix_new = temp;
-//}
-
-#pragma acc routine seq
-void swapPointers(double *&d_p_matrix, double *&d_p_matrix_new) {
-    double *temp = d_p_matrix;
-    d_p_matrix = d_p_matrix_new;
-    d_p_matrix_new = temp;
+//call with <<<1, 1>>> to swap pointers
+__global__ void swapPointers(double **d_p_matrix, double **d_p_matrix_new) {
+    double *temp = *d_p_matrix;
+    *d_p_matrix = *d_p_matrix_new;
+    *d_p_matrix_new = temp;
 }
 
-double gpu_psolve(double *d_p_matrix, double *d_p_matrix_new, const double *d_rs_matrix, const bool *d_fluid_mask,
-                  const uint8_t *d_boundary_type, const gridParams grid, const int num_iterations) {
+
+double gpu_psolve(double *p_matrix, double *p_matrix_new, const double *rs_matrix, const bool *fluid_mask,
+                  const uint8_t *boundary_type, const gridParams grid, const int num_iterations) {
 
     double dx = grid.dx;
     double dy = grid.dy;
@@ -88,36 +84,25 @@ double gpu_psolve(double *d_p_matrix, double *d_p_matrix_new, const double *d_rs
     double res = 0;
     int size_fluid_cells = grid.size_fluid_cells;
 
-    //present means data present on GPU
-    // JACOBI ITERATION
-    #pragma acc loop seq
+    double **d_p_matrix, **d_p_matrix_new;
+    cudaMalloc(&d_p_matrix, sizeof(double *));
+    cudaMalloc(&d_p_matrix_new, sizeof(double *));
+
+    cudaMemcpy(d_p_matrix, &p_matrix, sizeof(double *), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_p_matrix_new, &p_matrix_new, sizeof(double *), cudaMemcpyHostToDevice);
+
+    dim3 threadsPerBlock(8, 8);
+    dim3 numBlocks((imax + 2 + threadsPerBlock.x - 1) / threadsPerBlock.x, (jmax + 2 + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
     for (int iter = 0; iter < num_iterations; iter++) {
-        #pragma acc parallel loop collapse(2) vector_length(128) \
-        present(d_rs_matrix[0 : size_linear], d_fluid_mask[0 : size_linear], d_boundary_type[0 : size_linear]) \
-        deviceptr(d_p_matrix_new, d_p_matrix)
-        for (int i = 1; i <= imax; i++) {
-            for (int j = 1; j <= jmax; j++) {
-                int idx = i + j * (imax + 2);
-                int idx_left = idx - 1;
-                int idx_right = idx + 1;
-                int idx_top = idx + (imax + 2);
-                int idx_bottom = idx - (imax + 2);
-
-                d_p_matrix_new[idx] =
-                    coeff * ((d_p_matrix[idx_left] + d_p_matrix[idx_right]) / (dx * dx) +
-                             (d_p_matrix[idx_top] + d_p_matrix[idx_bottom]) / (dy * dy) - d_rs_matrix[idx]);
-                d_p_matrix_new[idx] *= d_fluid_mask[idx]; // set to zero if not fluid cell
-            }
-        }
-
-        swapPointers(d_p_matrix, d_p_matrix_new);
-
+        jacobiKernel<<<numBlocks, threadsPerBlock>>>(d_p_matrix_new, d_p_matrix, rs_matrix, fluid_mask, coeff, imax, jmax, dx, dy);
+        swapPointers<<<1, 1>>>(d_p_matrix, d_p_matrix_new);
     }
 
     //TODO apply boundary conditions
 
     // CALCULATE RESIDUAL
-    #pragma acc parallel loop collapse(2) reduction(+:res) present(d_p_matrix[0:size_linear], d_rs_matrix[0:size_linear], d_fluid_mask[0:size_linear])
+    #pragma acc parallel loop collapse(2) reduction(+:res) present(p_matrix[0:size_linear], rs_matrix[0:size_linear], fluid_mask[0:size_linear])
     for (int i = 1; i <= imax; i++) {
         for (int j = 1; j <= jmax; j++) {
             int idx = i + j * (imax + 2);
@@ -126,12 +111,14 @@ double gpu_psolve(double *d_p_matrix, double *d_p_matrix_new, const double *d_rs
             int idx_top = idx + (imax + 2);
             int idx_bottom = idx - (imax + 2);
 
-            double val = (d_p_matrix[idx_left] - 2.0 * d_p_matrix[idx] + d_p_matrix[idx_right]) / (dx * dx) +
-                         (d_p_matrix[idx_bottom] - 2.0 * d_p_matrix[idx] + d_p_matrix[idx_top]) / (dy * dy) -
-                         d_rs_matrix[idx];
-            res += val * val * d_fluid_mask[idx];
+            double val = (p_matrix[idx_left] - 2.0 * p_matrix[idx] + p_matrix[idx_right]) / (dx * dx) +
+                         (p_matrix[idx_bottom] - 2.0 * p_matrix[idx] + p_matrix[idx_top]) / (dy * dy) - rs_matrix[idx];
+            res += val * val * fluid_mask[idx];
         }
     }
+
+    cudaFree(d_p_matrix);
+    cudaFree(d_p_matrix_new);
 
     return res / size_fluid_cells;
 
